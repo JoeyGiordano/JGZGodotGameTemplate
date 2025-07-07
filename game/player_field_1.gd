22ae715e979ab35074d4408ac7261ae59d0a3362
JoeyGiordano <jgzgiordano@gmail.com>
2025-07-06 23:53:40 -0700

    First Additions

--- game/player_field_1.gd
+++ game/player_field_1.gd
@@ -4,16 +4,21 @@ class_name PlayerField1
 @onready var label : Label = $Label
 @onready var message : Label = $MessageLabel
 @onready var rect : ColorRect = $ColorRect
+@onready var count_label : Label = $CountLabel
 @onready var button1 : Button = $Button
 @onready var button2 : Button = $Button2
+@onready var end_turn_button : Button = $EndTurnButton
 @onready var leave_button : Button = $LeaveButton
 
 func _ready():
 	button1.connect("button_down", _on_button1_pressed)
 	button2.connect("button_down", _on_button2_pressed)
+	end_turn_button.connect("button_down", _on_end_turn_button_pressed)
 	leave_button.connect("button_down", _on_leave_button_pressed)
 	$IDLabel.text = str(multiplayer.get_unique_id())
 	if multiplayer.get_unique_id() == 1 : leave_button.text = "End"
+
+## Signal response
 
 func _on_button1_pressed() :
 	if waiting_for_opponent() : return
@@ -31,11 +36,15 @@ func _on_button2_pressed() :
 	if waiting_for_opponent() : return
 	var rand_color = get_random_color()
 	rect.color = rand_color
+	count_label.text = str(int(count_label.text)+1)
 	rpc("_on_button2_pressed_for_peer", rand_color)
 
 @rpc("any_peer")
 func _on_button2_pressed_for_peer(color : Color) :
 	rect.color = color
+
+func _on_end_turn_button_pressed() :
+	pass
 
 func _on_leave_button_pressed() :
 	#if multiplayer.get_unique_id() == 1 : #host end button pressed
@@ -46,6 +55,8 @@ func _on_leave_button_pressed() :
 	
 	GameContainer.GC.destroy_opponent_scene()
 	GameContainer.GC.switch_to_scene("main_menu")
+
+## Resource
 
 func get_random_color() -> Color:
 	var hue = randf_range(0.0, 1.0)
