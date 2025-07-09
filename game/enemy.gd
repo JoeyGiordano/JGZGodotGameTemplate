6f4bfeeb76514cce7ce8669ae0c218ed6f9cba1c
JoeyGiordano <jgzgiordano@gmail.com>
2025-07-09 01:13:45 -0700

    Added enemy
    Next: spawning and motion

---
+++ game/enemy.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var sprite : Sprite2D = $Sprite2D
+
+@export var state : int = 0 #0 rock, 1 paper, 2 siscors
+
+func _process(delta):
+	rotation += -7 *delta
+	update_color()
+
+func die() :
+	rpc("_die")
+
+@rpc("call_local")
+func _die() :
+	sprite.modulate = Color.BLACK
+	state = 3 #so no death
+	await get_tree().create_timer(0.3).timeout
+	queue_free()
+
+func update_color() :
+	match(state) :
+		0 : sprite.modulate = Color.RED
+		1 : sprite.modulate = Color.GREEN
+		2 : sprite.modulate = Color.BLUE
--- game/player.gd
+++ game/player.gd
@@ -8,16 +8,23 @@ var cooldown : float = 1.3
 var in_play : bool = false
 @export var state : int = 0 #0 rock, 1 paper, 2 siscors
 @export var switch_ready : bool = true
+var dead : bool = false
+var game_over : bool = false
 
 func _ready():
 	update_color()
+	$Area2D.area_entered.connect(_on_area_entered)
 
 func _process(delta):
 	if !is_multiplayer_authority() : return
 	
+	rotation += delta * 3 #spin
+	
 	if !in_play : return
 	
-	update_color()
+	rotation += delta * 4 #SPIN FASTER
+	
+	if !dead : update_color()
 	
 	var movement : Vector2 = Vector2(Input.get_axis("left","right"), Input.get_axis("up","down"))
 	movement = movement.normalized()
@@ -37,3 +44,37 @@ func update_color() :
 		0 : sprite.modulate = Color.RED
 		1 : sprite.modulate = Color.GREEN
 		2 : sprite.modulate = Color.BLUE
+
+func die() :
+	if game_over : return
+	rpc("game_over_")
+	if dead : return
+	dead = true
+	state = 3 #so no death
+	sprite.modulate = Color.BLACK
+	await get_tree().create_timer(3).timeout
+	rpc("restart")
+
+@rpc("call_local")
+func game_over_() :
+	game_over = true
+
+@rpc("call_local")
+func restart() :
+	#start the scenes again
+	GameContainer.GC.switch_to_scene("player_field_1")
+	GameContainer.GC.switch_to_opponent_scene("opponent_field_1")
+
+func _on_area_entered(area : Area2D) :
+	var statex = area.get_parent().state
+	if state == 0 :
+		if statex == 1 : die()
+		if statex == 2 : area.get_parent().die()
+	if state == 1 :
+		if statex == 2 : die()
+		if statex == 0 : area.get_parent().die()
+	if state == 2 :
+		if statex == 0 : die()
+		if statex == 1 : area.get_parent().die()
+			
+			
