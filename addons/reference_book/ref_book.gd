64bc683eaa196f80c8f27776c85cdc67dde059db
JoeyGiordano <74273004+JoeyGiordano@users.noreply.github.com>
2026-01-27 21:52:32 -0500

    4.6 addon

--- addons/reference_book/ref_book.gd
+++ addons/reference_book/ref_book.gd
@@ -2,8 +2,9 @@
 extends EditorPlugin
 
 const window_scene: PackedScene = preload("./path_manager/path_manager_menu.tscn")
-var toolbar
+var dock : EditorDock
 var window : Window
+var dock_content
 
 func _enable_plugin() -> void:
 	pass
@@ -13,20 +14,21 @@ func _disable_plugin() -> void:
 
 
 func _enter_tree() -> void:
-	toolbar = preload("res://addons/reference_book/Control.tscn").instantiate()
-	
-	#add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, toolbar)
-	#add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BL, toolbar)
-	add_control_to_bottom_panel(toolbar, "Ref Book")
+	dock = EditorDock.new()
+	dock.title = "Ref Book"
+	dock.default_slot = EditorDock.DOCK_SLOT_BOTTOM
+	dock.available_layouts = EditorDock.DOCK_LAYOUT_ALL
+	dock_content = preload("res://addons/reference_book/Control.tscn").instantiate()
+	dock.add_child(dock_content)
+	add_dock(dock)
 	
 	# path manager
 	add_tool_menu_item("Reference Book...", create_window)
 
 func _exit_tree() -> void:
-	#remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, toolbar)
-	#remove_control_from_docks(toolbar)
-	remove_control_from_bottom_panel(toolbar)
-	toolbar.free()
+	remove_dock(dock)
+	dock.queue_free()
+	dock = null
 	
 	#path manager
 	remove_tool_menu_item("Reference Book...")
@@ -35,6 +37,6 @@ func _exit_tree() -> void:
 func create_window() -> void:
 	window = window_scene.instantiate()
 	EditorInterface.get_base_control().add_child(window)
-	window.toolbar = toolbar
+	window.dock_content = dock_content
 	window.init()
 	
